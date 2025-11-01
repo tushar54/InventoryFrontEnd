a730a4f8d5ac7b687c173c52d084345e152e37aa
tushar54 <tusharahmed153216@gmail.com>
2025-11-01 22:17:11 +0600

    completed

--- server.js
+++ server.js
@@ -7,14 +7,16 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Serve static files from Angular's dist directory
-app.use(express.static(path.join(__dirname, "dist/inventory_FrontEnd/browser")));
+// Correct path to Angular build
+const distPath = path.join(__dirname, "dist/inventory_FrontEnd");
 
-// Redirect all routes to index.html
+// Serve static files
+app.use(express.static(distPath));
+
+// Catch all routes
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/inventory_FrontEnd/browser/index.html"));
+  res.sendFile(path.join(distPath, "index.html"));
 });
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
